3df9b45b438452507257fcf82140d77c5d5491b6
muhamad <muhamad.zolfaghari@gmail.com>
2024-01-08 23:13:50 +0330

    refactor library

--- src/useBodyDirection.ts
+++ src/useBodyDirection.ts
@@ -3,12 +3,11 @@ import * as React from "react";
 type DirectionType = "rtl" | "ltr" | string;
 
 const useBodyDirection = () => {
-  const prevDirection = React.useRef<DirectionType>();
-
+  const directionRef = React.useRef<DirectionType>();
   const [direction, setDirection] = React.useState<DirectionType>();
 
   React.useEffect(() => {
-    prevDirection.current = direction;
+    directionRef.current = direction;
   }, [direction]);
 
   React.useEffect(() => {
@@ -19,14 +18,14 @@ const useBodyDirection = () => {
     function handleStyleChanged(mutations: MutationRecord[]) {
       const newDirection = (mutations[0].target as HTMLBodyElement).style.direction;
 
-      if (newDirection !== prevDirection.current) {
+      if (newDirection !== directionRef.current) {
         setDirection(document.body.style.direction);
       }
     }
 
     const observer = new MutationObserver(handleStyleChanged);
     observer.observe(document.body, {
-      attributeFilter: ["style"],
+      attributeFilter: ["style"]
     });
 
     return () => {
